a03e236b77949813bb734c1fbe10126c94a17936
NIKMC <nikmc10@mail.ru>
2018-02-24 23:41:13 +0300

    Add item functionality and rename item functionality

--- Notes/ToDoListViewController.swift
+++ Notes/ToDoListViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    let itemArray = ["Note1", "Note2", "Note3"]
+    var itemArray = ["Note1", "Note2", "Note3"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(self.longPress(longPressGestureRecognizer:)))
+        self.view.addGestureRecognizer(longPressRecognizer)
     }
     
     //MARK - Tableview DataSource Methods
@@ -40,6 +42,60 @@ class ToDoListViewController: UITableViewController {
         
         
     }
-
+    //MARK - longpressed item
+    
+    @objc func longPress(longPressGestureRecognizer: UILongPressGestureRecognizer) {
+        
+        if longPressGestureRecognizer.state == UIGestureRecognizerState.began {
+            
+            let touchPoint = longPressGestureRecognizer.location(in: self.view)
+            let alert = UIAlertController(title: "Rename Item", message: "", preferredStyle: .alert)
+            
+            if let index = self.tableView.indexPathForRow(at: touchPoint)  {
+                
+                // your code here, get the row for the indexPath or do whatever you want
+                var textField = UITextField()
+                let action = UIAlertAction(title: "Rename", style: .default) { (action) in
+                    //what will happen once user clicks the Add Iten Button on UIAlert
+                    print("Success")
+                    if textField.text != nil {
+                        self.itemArray[index.row] = textField.text!
+                        self.tableView.reloadData()
+                    }
+                }
+                alert.addTextField{ (alertTextField) in
+                    alertTextField.text = self.itemArray[index.row]
+                    textField = alertTextField
+                }
+                alert.addAction(action)
+                present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    //MARK - Add new Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Notes Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once user clicks the Add Iten Button on UIAlert
+            print("Success")
+            if textField.text != nil {
+                self.itemArray.append(textField.text!)
+                self.tableView.reloadData()
+            }
+        }
+        alert.addTextField{ (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
 
