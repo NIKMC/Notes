ba34c673f16a4682c6239ad794eaaff654b62b3c
NIKMC <nikmc10@mail.ru>
2018-02-28 22:38:09 +0300

    bug fix

--- Notes/Controller/ToDoListViewController.swift
+++ Notes/Controller/ToDoListViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    var itemArray = ["Note1", "Note2", "Note3"]
-    
+//    var itemArray = ["Note1", "Note2", "Note3","Note11", "Note12", "Note13","Note21", "Note22", "Note23","Note31", "Note32", "Note33","Note41", "Note42", "Note43","Note51", "Note52", "Note53","Note61", "Note62", "Note63"]
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
-            itemArray = items
+        for i in 0...30 {
+            let item = Item()
+            item.title = "note \(i)"
+            itemArray.append(item)
         }
+        
+//        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+//            itemArray = items
+//        }
         
         let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(self.longPress(longPressGestureRecognizer:)))
         self.view.addGestureRecognizer(longPressRecognizer)
@@ -31,20 +37,29 @@ class ToDoListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+//        let cell = UITableViewCell(style: .default, reuseIdentifier: "ToDoItemCell")
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+
         return cell
     }
     
     //MARK - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+
+        tableView.reloadData()
+        
         tableView.deselectRow(at: indexPath, animated: true)
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        } else {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
         
         
     }
@@ -65,13 +80,13 @@ class ToDoListViewController: UITableViewController {
                     //what will happen once user clicks the Add Iten Button on UIAlert
                     print("Success")
                     if textField.text != nil {
-                        self.itemArray[index.row] = textField.text!
+                        self.itemArray[index.row].title = textField.text!
                         self.defaults.setValue(self.itemArray, forKey: "ToDoListArray")
                         self.tableView.reloadData()
                     }
                 }
                 alert.addTextField{ (alertTextField) in
-                    alertTextField.text = self.itemArray[index.row]
+                    alertTextField.text = self.itemArray[index.row].title
                     textField = alertTextField
                 }
                 alert.addAction(action)
@@ -91,7 +106,10 @@ class ToDoListViewController: UITableViewController {
             //what will happen once user clicks the Add Iten Button on UIAlert
             print("Success")
             if textField.text != nil {
-                self.itemArray.append(textField.text!)
+                let newItem = Item()
+                newItem.title = textField.text!
+            
+                self.itemArray.append(newItem)
                 self.defaults.setValue(self.itemArray, forKey: "ToDoListArray")
                 self.tableView.reloadData()
             }
---
+++ Notes/Model/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  Notes
+//
+//  Created by Иван Никитин on 28/02/2018.
+//  Copyright © 2018 Иван Никитин. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    public var title : String = ""
+    public var done : Bool = false
+    
+//    init(Title : String, checked : Bool) {
+//        self.title = Title
+//        self.done = checked
+//    }
+}
